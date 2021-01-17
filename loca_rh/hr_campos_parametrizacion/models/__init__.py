2263e815c21b1f932d994557ca2e4f65c31b278e
solucionestecnoweb <inmldrsolucionestecnologicas@gmail.com>
2021-01-16 20:20:42 -0400

    modulo rrhh

---
+++ loca_rh/hr_campos_parametrizacion/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import hr_contract_add_fields
+from . import hr_holidays_days
+from . import hr_bono_alimenticio
